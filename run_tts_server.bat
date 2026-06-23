504ba31371c4d3fd7e9451a5bcd4d9c0d04caa46
whenigetout <ajaykrsunki@gmail.com>
2026-06-23 19:14:58 +0530

    Sync contracts before server startup

--- run_tts_server.bat
+++ run_tts_server.bat
@@ -5,4 +5,5 @@ REM Change this to your Conda environment name
 CALL conda activate chatterbox
 
 REM Run the FastAPI app using uvicorn
+python scripts\sync_contracts.py
 uvicorn tts_server:app --host 0.0.0.0 --port 7861 --reload
